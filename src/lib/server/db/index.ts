776750bb0d09e8b64d4ef29d5d951437f9aa7f03
samiS2003 <sa.en@live.com>
2024-05-08 11:58:54 +0300

    vercel fix

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,5 +1,9 @@
+import { promises as fs } from 'fs';
 import { drizzle, type BetterSQLite3Database } from 'drizzle-orm/better-sqlite3'
 import { env } from '$env/dynamic/private'
 import Database from 'better-sqlite3'
-const sqlite = new Database(env.DB_PATH)
+
+const file = await fs.readFile(process.cwd() + env.DB_PATH, 'utf8');
+const sqlite = new Database(file)
+// export const db: BetterSQLite3Database = drizzle(sqlite)
 export const db: BetterSQLite3Database = drizzle(sqlite)
