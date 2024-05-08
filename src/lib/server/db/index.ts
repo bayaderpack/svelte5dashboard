3150fbecd062bcf76c109d3410e36006cd7a23f2
samiS2003 <sa.en@live.com>
2024-05-08 12:20:22 +0300

    change to pnpm

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,9 +1,5 @@
-import { promises as fs } from 'fs';
 import { drizzle, type BetterSQLite3Database } from 'drizzle-orm/better-sqlite3'
 import { env } from '$env/dynamic/private'
 import Database from 'better-sqlite3'
-
-const file = await fs.readFile(process.cwd() + env.DB_PATH, 'utf8');
-const sqlite = new Database(file)
-// export const db: BetterSQLite3Database = drizzle(sqlite)
+const sqlite = new Database(env.DB_PATH)
 export const db: BetterSQLite3Database = drizzle(sqlite)
